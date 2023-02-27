4782a0ed6c2bd25d52461deddbac4fd7b2f768f8
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-02-27 18:26:47 +0300

    Add signup, signin and signour routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from "./routes/current-user";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
