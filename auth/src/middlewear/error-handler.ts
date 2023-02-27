f3c5c3549cad78a24a0182c9372c0ab3211fdb3e
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-02-27 21:46:32 +0300

    Set response.message to err.message

--- auth/src/middlewear/error-handler.ts
+++ auth/src/middlewear/error-handler.ts
@@ -12,6 +12,6 @@ import { Request, Response, NextFunction } from "express";
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log('Something went wrong', err);
     res.status(400).send({
-        message: 'Something went wrong'
+        message: err.message
     });
 };
