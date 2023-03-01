889d7699ba9452cc40a9bed7e79dc44fcb2020d1
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-03-01 04:04:05 +0300

    Sign jwt token and send it back to client in cookie on sign up

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signupRouter } from "./routes/signup";
@@ -11,7 +12,12 @@ import { errorHandler } from "./middlewear/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set('trust proxy', true); // Ingress nginx is our proxy. We need to tell Express to trust it.
 app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}));
 
 app.use(currentUserRouter);
 app.use(signupRouter);
