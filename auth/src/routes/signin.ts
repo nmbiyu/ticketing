2647abc9086257507dff22f2bdccf193597ac46b
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-03-01 08:42:38 +0300

    Add /api/users/signin implementation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { validateRequest } from "../middlewear/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import { User } from "../models/user";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
@@ -13,8 +18,36 @@ router.post('/api/users/signin',
             .trim()
             .notEmpty()
             .withMessage("You must supply a password")
-    ], validateRequest, (req: Request, res: Response) => {
+    ], validateRequest, async (req: Request, res: Response) => {
 
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+        throw new BadRequestError("Invalid credentials");
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.get("password"), password);
+    if (!passwordsMatch) {
+        throw new BadRequestError("Invalid credentials");
+    }
+
+    // Generate JWT
+    // JWT Secret has been created in Kubernetes with the following command.
+    // kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
+    // kubectl get secrets can be used to list the secrets.
+    // We do not want to have a config file for this as it would expose the key.
+    const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+    }, process.env.JWT_KEY!); // The ! tells TypeScript that we have checked the type of process.env.JWT_KEY.
+                              // See check at index.ts#start
+
+    // Store it on session object
+    req.session = {
+        jwt: userJwt
+    };
+
+    return res.status(200).send(existingUser);
 });
 
 export { router as signinRouter };
