4675e30ebebe03b488cd64e0cbf987abf2995303
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-03-02 09:07:50 +0300

    Add bootstrap to client project

---
+++ client/pages/_app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+// See https://nextjs.org/docs/messages/css-global on importing Global CSS.
+const App = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />
+}
+export default App;
