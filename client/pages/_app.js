aa277d4882b44ec632e7afcddcb3f8bf0fc0dd1c
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-03-05 15:10:54 +0300

    Create getInitialProps function in App Component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
+import buildClient from '../api/build-client';
+
 // See https://nextjs.org/docs/messages/css-global on importing Global CSS.
-const App = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+const App = ({ Component, pageProps, currentUser }) => {
+    return (
+        <div>
+            <h1>Header! {currentUser.email}</h1>
+            <Component {...pageProps} />
+        </div>
+    );
 }
+
+App.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentuser');
+    const pageProps = appContext.Component.getInitialProps
+        ? await appContext.Component.getInitialProps(appContext.ctx)
+        : {};
+    return {
+        pageProps,
+        currentUser: data.currentUser
+    };
+};
+
 export default App;
