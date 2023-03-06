cee2ac8e1e537e1da32463478eff2d2825e3065d
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-03-06 10:15:08 +0300

    Create Header component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
 // See https://nextjs.org/docs/messages/css-global on importing Global CSS.
 const App = ({ Component, pageProps, currentUser }) => {
     return (
         <div>
-            <h1>Header! {currentUser.email}</h1>
+            <Header currentUser={currentUser} />
             <Component {...pageProps} />
         </div>
     );
