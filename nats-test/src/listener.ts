94cef522004fddd9d91c600e9dadc38de3d17b9e
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-03-31 10:39:26 +0300

    Inspect and log message in listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
 
 console.clear();
 
@@ -10,7 +10,11 @@ stan.on('connect', () => {
     console.log('Listener connected to NATS');
 
     const subscription = stan.subscribe('ticket:created');
-    subscription.on('message', (msg) => {
-        console.log('Message received');
+    subscription.on('message', (msg: Message) => {
+        const data = msg.getData();
+
+        if (typeof data === 'string') {
+            console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+        }
     });
 });
