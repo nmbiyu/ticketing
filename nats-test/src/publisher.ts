d02cf9a78bbe280119334daec7ca67b28f73f834
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-03-31 10:27:42 +0300

    Connect listener to nats server

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,7 @@
 import nats from 'node-nats-streaming';
 
+console.clear();
+
 const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222' // NATS server running in the k8s cluster.
 });
