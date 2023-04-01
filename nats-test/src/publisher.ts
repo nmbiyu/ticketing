3f94ae6ecc8d33772795e317f4c03b080182406e
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-01 15:04:39 +0300

    Create base listener class

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
@@ -9,14 +10,10 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
     console.log('Publisher connected to NATS');
 
-    // To publish data to the nats streaming server, our Object needs to be converted to JSON.
-    const data = JSON.stringify({
+    const publisher = new TicketCreatedPublisher(stan);
+    publisher.publish({
         id: '123',
         title: 'concert',
         price: 20
-    });
-
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published');
     });
 });
