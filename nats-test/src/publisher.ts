21467e8988a8b4b710842fa4d45f4b6f7eaf8d94
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-01 15:09:56 +0300

    Await event publication

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,11 +7,11 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222' // NATS server running in the k8s cluster.
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
 
     const publisher = new TicketCreatedPublisher(stan);
-    publisher.publish({
+    await publisher.publish({
         id: '123',
         title: 'concert',
         price: 20
