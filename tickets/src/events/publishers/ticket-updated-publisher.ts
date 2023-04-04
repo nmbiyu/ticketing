046cf78b7692e4bc8b1c4f59e279cb2f9574447d
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-04 04:52:39 +0300

    Add TicketUpdatePublisher

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@nmbiyutickets/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
