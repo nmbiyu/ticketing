7fe2434ddbc2095bd7d75cbf84878b87ab85679f
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-04 20:20:01 +0300

    Create Order and Ticket models in orders service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,19 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import { requireAuth , validateRequest } from "@nmbiyutickets/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', [
+    body('ticketId')
+        .not()
+        .isEmpty()
+        // Verify that the incoming id is a valid mongodb id. The ticket service may not use mongodb so this is not
+        // something that is typical of production systems. However, for learning purposes, we'll tolerate it.
+        .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+        .withMessage('TicketId must be provided')
+], requireAuth, async (req: Request, res: Response) => {
     res.send({});
 });
 
