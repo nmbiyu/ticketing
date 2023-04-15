a4ee22d1f1b2d4b4a826c222b5b1c1efa7caf106
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-15 12:26:07 +0300

    Set an orderId to the ticket when an order is created

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { OrderCreatedEvent, OrderStatus } from "@nmbiyutickets/common";
+
+import { OrderCreatedListener } from "../order-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+    // Create an instance of the listener.
+    const listener = new OrderCreatedListener(natsWrapper.client);
+
+    // Create and save a ticket.
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 10,
+        userId: new mongoose.Types.ObjectId().toHexString()
+    });
+    await ticket.save();
+
+    // Create the fake data event.
+    const data: OrderCreatedEvent['data'] = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        status: OrderStatus.Created,
+        userId: ticket.userId,
+        expiresAt: new Date(new Date().getTime() + 10 * 60 * 60).toISOString(),
+        ticket: {
+            id: ticket.id,
+            price: ticket.price
+        }
+    };
+
+    // Create a fake msg Object. A Message instance has more properties that we do not care for. Use the @ts-ignore
+    // annotation to tell TypeScript to ignore the missing properties.
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    };
+
+    return { listener, ticket, data, msg };
+};
+
+it('sets the user id of the ticket', async () => {
+    const { listener, ticket, data, msg } = await setup();
+    await listener.onMessage(data, msg);
+
+    const updatedTicket = await Ticket.findById(ticket.id);
+    expect(updatedTicket!.orderId).toEqual(data.id);
+});
+
+it('acks the message', async () => {
+    const { listener, ticket, data, msg } = await setup();
+    await listener.onMessage(data, msg);
+
+    await Ticket.findById(ticket.id);
+    expect(msg.ack).toHaveBeenCalled();
+});
---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from "node-nats-streaming";
+
+import { Listener, OrderCreatedEvent, Subjects } from "@nmbiyutickets/common";
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+    queueGroupName: string = queueGroupName;
+
+    async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+        const ticket = await Ticket.findById(data.ticket.id);
+
+        if (!ticket) {
+            throw new Error('Ticket not found');
+        }
+
+        ticket.set({ orderId: data.id });
+        await ticket.save();
+
+        msg.ack();
+    }
+}
