9943e770221b28b6fae28918d31a76bc4820201c
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-15 13:02:02 +0300

    Emit TicketUpdatedEvent when an order is created

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -3,6 +3,7 @@ import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@nmbiyutickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -17,6 +18,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
         ticket.set({ orderId: data.id });
         await ticket.save();
+        await new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            price: ticket.price,
+            title: ticket.title,
+            userId: ticket.userId,
+            orderId: data.id,
+            version: ticket.version
+        });
 
         msg.ack();
     }
