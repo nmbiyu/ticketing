0f9a73b319941c06f5c208b4ed18f00f91d0b72b
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-16 16:20:09 +0300

    Add PaymentCreatedListener in orders service

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from "node-nats-streaming";
+import { Listener, OrderStatus, PaymentCreatedEvent, Subjects } from "@nmbiyutickets/common";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+    subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+    queueGroupName: string;
+
+    async onMessage(data: PaymentCreatedEvent["data"], msg: Message) {
+        const order = await Order.findById(data.orderId);
+
+        if (!order) {
+            throw new Error('Order not found');
+        }
+
+        // An order in the Complete status is in a terminal state, i.e. no further changes can be made to it. Therefore,
+        // we can live with not having to emit an event for this update. However, it would be good to emit the event so
+        // that downstream services with copies of order records are in a consistent state.
+        order.set({ status: OrderStatus.Complete });
+        msg.ack();
+    }
+}
