526cf868378a8ee0287622d5dce37b4c26864358
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-16 13:57:00 +0300

    Add POST /api/payments endpoint

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from "cookie-session";
 import { currentUser } from "@nmbiyutickets/common";
+import { createChargeRouter } from "./routes/new";
 
 const app = express();
 app.set('trust proxy', true); // Ingress nginx is our proxy. We need to tell Express to trust it.
@@ -12,5 +13,7 @@ app.use(cookieSession({
     secure: process.env.NODE_ENV !== 'test'
 }));
 app.use(currentUser);
+
+app.use(createChargeRouter);
 
 export { app };
