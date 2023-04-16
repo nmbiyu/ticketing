dc8cd9427370fb92dbf9547360b235db08df6e63
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-16 15:58:01 +0300

    Record Payment when a Stripe charge is made

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
 } from "@nmbiyutickets/common";
 import { stripe } from "../stripe";
 import { Order } from "../models/order";
+import { Payment } from "../models/payment";
 
 const router = express.Router();
 
@@ -38,12 +39,18 @@ router.post('/api/payments',
         throw new BadRequestError('Cannot pay for a cancelled order');
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
         currency: 'usd',
         amount: order.price * 100,
         description: "Test charge made by Ndũng'ũ",
         source: token
     });
+    // TODO Handle charge failure. I think this assumes the Stripe API call always succeeds.
+    const payment = Payment.build({
+        orderId,
+        stripeId: charge.id
+    });
+    await payment.save();
 
     res.status(201).send({ success: true });
 });
