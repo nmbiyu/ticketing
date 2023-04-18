de061dd3e7b0e7d517659cd16f47de486d69c2b6
Ndũng'ũ Mbiyũ <jimmymbiyu@gmail.com>
2023-04-18 11:37:24 +0300

    Add OrderShow component
    The react-stripe-checkout lib threw the error noted in the link below.
    https://github.com/azmenak/react-stripe-checkout/issues/109. I have added followed this comment to work around the
    error. https://github.com/azmenak/react-stripe-checkout/issues/109#issuecomment-394464480

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,17 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import useRequest from "../../hooks/use-request";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
     const [timeLeft, setTimeLeft] = useState(0);
+    const { doRequest, errors } = useRequest({
+        url: '/api/payments',
+        method: 'post',
+        body: {
+            orderId: order.id
+        },
+        onSuccess: (payment) => console.log(payment)
+    });
 
     useEffect(() => {
         const findTimeLeft = () => {
@@ -23,7 +33,16 @@ const OrderShow = ({ order }) => {
     }
 
     return (
-        <div>{timeLeft} seconds until order expires</div>
+        <div>
+            {timeLeft} seconds until order expires
+            <StripeCheckout
+                token={({ id }) => doRequest({ token: id })}
+                stripeKey="pk_test_51GyHl2GdilfQJ5GVwen4iXwQvKbEXUCFZSBv2e1Ay5oeJoP1HyysnCQ5fGqHryeneOxSpsUyJOnFnfAfou201gKh00YVotZ139"
+                amount={order.ticket.price * 100}
+                email={currentUser.email}
+            />
+            {errors}
+        </div>
     );
 };
 
